8cd7794dac56521470404de13fb6c577cd972f43
shadow-shield <shadowgit863@gmail.com>
2024-11-14 21:33:52 -0500

    dasboard actualizado

--- src/pages/AdmitidosPorTipo.jsx
+++ src/pages/AdmitidosPorTipo.jsx
@@ -4,12 +4,12 @@ import GraficaAdmitidos from "../components/GraficaAdmitidosPorTipo";
 const AdmitidosPorTipo = () => {
   return (
     <div >
-      <h1 style={{textAlign:"center"}}>
+      <h1 style={{ textAlign: "center" }}>
       </h1>
       <h1 className="text-4xl font-bold text-center text-green-800 mt-8 mb-4">
-      ADMITIDOS CLASIFICADOS POR TIPO DE ADMISIÓN
-            </h1>
-      
+        ADMITIDOS CLASIFICADOS POR TIPO DE ADMISIÓN
+      </h1>
+
 
       <div style={{ marginTop: "2rem" }}>
         <GraficaAdmitidos />
